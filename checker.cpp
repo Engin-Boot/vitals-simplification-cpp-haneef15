1659e8e43ad05b000095d177a4496d4ec90520f0
haneef15 <51503680+haneef15@users.noreply.github.com>
2020-08-27 11:11:41 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,21 @@
 #include <assert.h>
 
+bool bpmisOk(float bpm)
+{
+  return(bpm < 70 || bpm > 150);
+}
+
+bool spo2isOk(float spo2)
+{
+  return(spo2 < 90);
+}
+bool respRateisOk(float resprate)
+{
+  return(respRate < 30 || respRate > 95);
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+  return(bpmisOk(bpm)&&spo2isOk(spo2)&&respRateisOk(resprate))
 }
 
 int main() {
