80fd8068a97ef206986b52a1cab0fdc74dea5f69
haneef15 <51503680+haneef15@users.noreply.github.com>
2020-08-27 12:13:20 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -5,7 +5,7 @@ bool isInsideLimit(float value,int lowerlimit,int upperlimit)
   return(value>=lowerlimit && value<=upperlimit)
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return(isInsideLimit(bpm,70,150) && isInsideLimit(spo2,90,100) && isInsideLimit(respRate,40,60));
+  return(isInsideLimit(bpm,70,150) && isInsideLimit(spo2,90,100) && isInsideLimit(respRate,30,95));
 }
 
 int main() {
