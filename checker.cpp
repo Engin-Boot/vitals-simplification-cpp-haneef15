9f262238b4689588f650cc8e1f9c002c8be14ab0
haneef15 <51503680+haneef15@users.noreply.github.com>
2020-08-28 11:45:07 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,11 +1,49 @@
 #include <assert.h>
+#include <iostream>
 
-bool isInsideLimit(float value,int lowerlimit,int upperlimit)
+using namespace std;
+
+class AlertwithSMS
 {
-  return(value>=lowerlimit && value<=upperlimit);
+  public:
+    void raiseAlert(cont char* VitalName, cont char* level)
+    {
+      cout<<"SMS" << VitalName << " " << level << endl;
+    }
+};
+
+class AlertwithSound
+{
+  public:
+    void raiseAlert(cont char* VitalName, cont char* level)
+    {
+      cout<<"Sound" << VitalName << " " << level << endl;
+    }
+};
+
+bool isInsideLimit(char* name,float value,int lowerlimit,int upperlimit)
+{
+  AlertwithSount *alert_with_sound = new AlertwithSound;
+  AlertwithSMS *alert_with_sms = new AlerwithSMS;
+  if(value>=lowerlimit && value<=upperlimit)
+  {
+     return true;
+  }
+  else if(value< lowerlimt)
+  {
+     alert_with_sound.raiseAlert(name,"is low");
+     alert_with_sms.raiseAlert(name,"is low");
+     return false;
+  }
+  else if(value >upperlimitlimt)
+  {
+     alert_with_sound.raiseAlert(name,"is high");
+     alert_with_sms.raiseAlert(name,"is high");
+     return false;
+  }
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return(isInsideLimit(bpm,70,150) && isInsideLimit(spo2,90,100) && isInsideLimit(respRate,30,95));
+  return(isInsideLimit("bpm",bpm,70,150) && isInsideLimit("spo2",spo2,90,100) && isInsideLimit("respRate",respRate,30,95));
 }
 
 int main() {
